0bed7d36b9949c841b33ff19874c90e2a780f894
Timtechs <Timmy.s.deckert@gmail.com>
2022-09-24 14:36:15 +0000

    forgot to save changes

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+computerBoard = [0] * 50
+playerBoard = [0] * 50
+
+computerBoardCopy = [0] * 50
+
+computerShot = []
+playerShot = []
+
+def main():
+    playerHit = 0
+    computerHit = 0
+
+    boatPosition(playerBoard, computerBoard)
+
+def boatPosition(length, x, y, direction, grid):
+    if direction == 0:
+        for i in range(length):
+            position = x + 5*y
+            grid.pop(position)
+            grid.insert(position, 1)
+            position = position + 1
+
+    if direction == 1:
+        for i in range(length):
+            position = x + 5*y
+            grid.pop(position)
+            grid.insert(position, 1)
+            position = position + 5
