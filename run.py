9e65e757828c259e11ccf8fec3f3d5ca14cd3915
Timtechs <Timmy.s.deckert@gmail.com>
2022-09-24 15:11:04 +0000

    Add main game functions

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ playerBoard = [0] * 50
 
 computerBoardCopy = [0] * 50
 
+onGame = True
+
 computerShot = []
 playerShot = []
 
@@ -13,6 +15,41 @@ def main():
     computerHit = 0
 
     boatPosition(playerBoard, computerBoard)
+
+    while onGame == True:
+
+        print(computerBoardCopy)
+
+        playerShot = input("Choose where to shoot")
+        playerShot.split(",")
+        letter = playerShot[0]
+        number = int(playershot[2])
+
+        player_Shot = int(ord(letter)) - 65 + 10*(number - 1)
+
+        if can_shoot(prevPlayerShot, player_shot) == True:
+
+            if computerShot[player_Shot] == 1:
+                hit(computerBoardCopy, player_Shot)
+                print("Hit!")
+                boatHitPlayer = boatHit + 1
+                end_Game("Player")
+
+            else:
+                shoot(computerBoardCopy, player_Shot)
+                print("Aww.. you missed.. Computers turn: ")
+                computer_Shot = random.randint(0,49)
+                
+                if playerBoard[computer_Shot] == 1:
+                    print("Computer Hit!")
+                    hit(playerBoard, computer_Shot)
+                    print(playerBoard)
+                    boatHitComputer = boatHitComputer + 1
+                    end_game("Computer")
+
+                else:
+                    shoot(playerBoard, computer_Shot)
+                    pint("Computer missed!")
 
 def boatPosition(length, x, y, direction, grid):
     if direction == 0:
