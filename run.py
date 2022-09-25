2758f8c50be1b4a959c1d6c597c4d0bf00cd259e
Timtechs <Timmy.s.deckert@gmail.com>
2022-09-25 10:22:49 +0000

    Restarted code

--- run.py
+++ run.py
@@ -1,119 +1,47 @@
+"""
+Imports and global variables
+"""
 import random
 
-computerBoard = [0] * 100
-playerBoard = [0] * 100
+BOARD_COLUMNS_SIZE = 3
+BOARD_ROW_SIZE = 1
 
-computerBoardCopy = [0] * 100
-
-ONGAME = True
-
-computerShot = []
-playerShot = []
+player_board = [0, 0, 0]
+computer_board = [0, 0, 0]
 
 
 def main():
-    player_Hit = 0
-    compHit = 0
+    """
+    Function with game logic and positioning. With user input for both
+    ship placement and shots.
+    """
+    computer_ship_loc = random.randint(0, BOARD_COLUMNS_SIZE)
+    computer_board[computer_ship_loc] = 0
+    user_ship_loc = input("Enter a value from 0-2 to place your ship: ")
 
-    boatPositions(playerBoard)
-    boatPositions(computerBoard)
+    if user_ship_loc in [0, 1, 2]:
+        return user_ship_loc
+    else:
+        user_ship_loc = input("Enter a value from 0-2 to place your ship: ")
 
-    while ONGAME:
+    player_board[user_ship_loc] = user_ship_loc
 
-        print(computerBoardCopy)
+    user_shot_locs = []
 
-        playerShot = input("Choose where to shoot")
-        playerShot.split(",")
-        letter = playerShot[0]
-        number = int(playerShot[2])
+    user_shot_loc = input("Enter a value from 0-2 to shoot computer ship: ")
 
-        player_Shot = int(ord(letter)) - 65 + 10*(number - 1)
+    if user_ship_loc not in [0, 1, 2]:
+        user_shot_loc = input("Enter a value from 0-2 to shoot comp ship: ")
 
-        if can_shoot(player_Hit, playerShot) is True:
+    if user_shot_loc not in user_shot_locs:
+        user_shot_locs.append(user_shot_loc)
 
-            if computerShot[player_Shot] == 1:
-                hit(computerBoardCopy, player_Shot)
-                print("Hit!")
-                player_Hit = player_Hit + 1
-                end_Game("Player")
+    if computer_board[user_ship_loc] == user_ship_loc:
+        print("Victory!")
 
-            else:
-                shoot(computerBoardCopy, player_Shot)
-                print("Aww.. you missed.. Computers turn: ")
-                computer_Shot = random.randint(0, 99)
-
-                if playerBoard[computer_Shot] == 1:
-                    print("Computer Hit!")
-                    hit(playerBoard, computer_Shot)
-                    print(playerBoard)
-                    compHit = compHit + 1
-                    end_Game("Computer")
-                else:
-                    shoot(playerBoard, computer_Shot)
-                    print("Computer missed!")
-        else:
-            print("error")
-
-
-def hit(grid, shot):
-    grid.pop(shot)
-    grid.insert(shot, "@")
-
-
-def shoot(grid, shot):
-    grid.pop(shot)
-    grid.insert(shot, "X")
-
-
-def end_Game(grid):
-    if grid == "Computer":
-        if grid == 14:
-            ONGAME = False
-            print("Sorry you lost")
-
-    elif grid == "Player":
-        if grid == 14:
-            ONGAME = False
-            print("Victory!")
-
-
-def can_shoot(List, shot):
-    for x in range(len(list)):
-        if List[x] == shot:
-            print("You struck here already")
-            return False
-    List.append(shot)
-    return True
-
-
-def boatPositions(grid):
-    for n in range(2, 6):
-        x, direction = random.randint(0, 9), random.randint(0, 9),
-        random.randint(0, 1)
-        while cant_Place_Boat(n, x, direction, grid):
-            x, direction = random.randint(0, 9), random.randint(0, 9),
-            random.randint(0, 1)
-        boatPosition(n, x, direction, grid)
-
-
-def boatPosition(length, x, direction, grid):
-    if direction == 0:
-        for i in range(length):
-            position = x + 10
-            grid.pop(position)
-            grid.insert(position, 1)
-            position = position + 1
-
-    if direction == 1:
-        for i in range(length):
-            position = x + 10
-            grid.pop(position)
-            grid.insert(position, 1)
-            position = position + 10
-
-
-def cant_Place_Boat(length, x, direction, grid):
-    return direction == 0 and x + length >= 10 or direction == 1
+    computer_shot_loc = random.randint(0, BOARD_COLUMNS_SIZE)
+    if player_board[computer_shot_loc] == user_ship_loc:
+        print("You lose!")
 
 
 main()
