3ad6d1bf7f30a2dfb31eaeca5b2ce2f02febd912
Timtechs <Timmy.s.deckert@gmail.com>
2023-01-03 20:33:04 +0000

    Remade code to fit the video

--- run.py
+++ run.py
@@ -1,47 +1,94 @@
-"""
-Imports and global variables
-"""
-import random
-
-BOARD_COLUMNS_SIZE = 3
-BOARD_ROW_SIZE = 1
-
-player_board = [0, 0, 0]
-computer_board = [0, 0, 0]
+from random import randint
 
 
-def main():
+scores = {"computer": 0, "player": 0}
+
+
+class Board:
     """
-    Function with game logic and positioning. With user input for both
-    ship placement and shots.
+    Main board class. Sets board size, the number of ships,
+    the player's name and the board type (player board or computer)
+    Has methods for adding ships and guesses and printing the board
     """
-    computer_ship_loc = random.randint(0, BOARD_COLUMNS_SIZE)
-    computer_board[computer_ship_loc] = 0
-    user_ship_loc = input("Enter a value from 0-2 to place your ship: ")
-
-    if user_ship_loc in [0, 1, 2]:
-        return user_ship_loc
-    else:
-        user_ship_loc = input("Enter a value from 0-2 to place your ship: ")
-
-    player_board[user_ship_loc] = user_ship_loc
-
-    user_shot_locs = []
-
-    user_shot_loc = input("Enter a value from 0-2 to shoot computer ship: ")
-
-    if user_ship_loc not in [0, 1, 2]:
-        user_shot_loc = input("Enter a value from 0-2 to shoot comp ship: ")
-
-    if user_shot_loc not in user_shot_locs:
-        user_shot_locs.append(user_shot_loc)
-
-    if computer_board[user_ship_loc] == user_ship_loc:
-        print("Victory!")
-
-    computer_shot_loc = random.randint(0, BOARD_COLUMNS_SIZE)
-    if player_board[computer_shot_loc] == user_ship_loc:
-        print("You lose!")
 
 
-main()
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+
+    def print(self):
+        for row in self.board:
+            print(" ".join(row))
+
+    def guess(self, x, y):
+        self.guesses.append((x, y))
+        self.board [x] [y] = "X"
+
+        if (x, y) in self.ships:
+            self.board [x] [y] = "*"
+            return "Hit"
+        else:
+            return "Miss"
+
+    def add_ship(self, x, y, type="computer"):
+        if len(self.ships) >= self.num_ships:
+            print("Error: you cannot add any more ships!")
+        else:
+            self.ships.append((x, y))
+            if self.type == "player":
+                self.board [x] [y] = "@"
+
+
+def random_point(size):
+    """
+    Helper function to return a random interger between 0 and size
+    """
+    return randint(0, size - 1)
+
+
+def valid_coordinates(x, y, board):
+
+
+def populate_board(board):
+
+
+def make_guess(board):
+
+
+def play_game(computer_board, player_board):
+
+
+def new_game():
+    """
+    Starts a new game. Sets the board size and number of ships, resets the
+    scores and initialises the boards.
+    """
+
+    size = 5
+    num_ships = 4
+    scores ["computer"] = 0
+    scores ["player"] = 0
+    print("-" * 35)
+    print(" Welcome to Timtechs Battleships")
+    print(f"Board Size: {size}. Number of ships: {num_ships}")
+    print(" Top left corner is row: 0, col: 0")
+    print("-" * 35)
+    player_name = input("Please enter your name: \n")
+    print("-" * 35)
+
+    computer_board = Board(size, num_ships, "Computer", type="computer")
+    player_board = Board(size, num_ships, player_name, type="player")
+    
+    for _ in range(num_ships):
+        populate_board(player_board)
+        populate_board(computer_board)
+    
+    play_game(computer_board, player_board)
+
+
+new_game()
