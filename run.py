07b48ba74184e3045bffe04447d48dc23db9ca30
Timtechs <Timmy.s.deckert@gmail.com>
2023-01-04 17:54:19 +0000

    Added function for gameboard, ship placement, player guess, update board and main.

--- run.py
+++ run.py
@@ -1,94 +1,26 @@
 from random import randint
 
 
-scores = {"computer": 0, "player": 0}
+def game_board(size):
+    return [['-' for count in range(size)] for count in range(size)]
 
 
-class Board:
-    """
-    Main board class. Sets board size, the number of ships,
-    the player's name and the board type (player board or computer)
-    Has methods for adding ships and guesses and printing the board
-    """
+def print_board(board):
+    for i in board:
+        print(*i)
 
 
-    def __init__(self, size, num_ships, name, type):
-        self.size = size
-        self.board = [["." for x in range(size)] for y in range(size)]
-        self.num_ships = num_ships
-        self.name = name
-        self.type = type
-        self.guesses = []
-        self.ships = []
-
-    def print(self):
-        for row in self.board:
-            print(" ".join(row))
-
-    def guess(self, x, y):
-        self.guesses.append((x, y))
-        self.board [x] [y] = "X"
-
-        if (x, y) in self.ships:
-            self.board [x] [y] = "*"
-            return "Hit"
-        else:
-            return "Miss"
-
-    def add_ship(self, x, y, type="computer"):
-        if len(self.ships) >= self.num_ships:
-            print("Error: you cannot add any more ships!")
-        else:
-            self.ships.append((x, y))
-            if self.type == "player":
-                self.board [x] [y] = "@"
+def place_ship(size):
+    row = random.randint(0, size - 1)
+    column = random.randint(0, size - 1)
+    coords = row, column
+    return list(coords)
 
 
-def random_point(size):
-    """
-    Helper function to return a random interger between 0 and size
-    """
-    return randint(0, size - 1)
+def main():
+    board = game_board(5)
+    print_board(board)
+    ship = place_ship(4); ship
 
 
-def valid_coordinates(x, y, board):
-
-
-def populate_board(board):
-
-
-def make_guess(board):
-
-
-def play_game(computer_board, player_board):
-
-
-def new_game():
-    """
-    Starts a new game. Sets the board size and number of ships, resets the
-    scores and initialises the boards.
-    """
-
-    size = 5
-    num_ships = 4
-    scores ["computer"] = 0
-    scores ["player"] = 0
-    print("-" * 35)
-    print(" Welcome to Timtechs Battleships")
-    print(f"Board Size: {size}. Number of ships: {num_ships}")
-    print(" Top left corner is row: 0, col: 0")
-    print("-" * 35)
-    player_name = input("Please enter your name: \n")
-    print("-" * 35)
-
-    computer_board = Board(size, num_ships, "Computer", type="computer")
-    player_board = Board(size, num_ships, player_name, type="player")
-    
-    for _ in range(num_ships):
-        populate_board(player_board)
-        populate_board(computer_board)
-    
-    play_game(computer_board, player_board)
-
-
-new_game()
+main()
