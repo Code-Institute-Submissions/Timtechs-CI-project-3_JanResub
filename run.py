16e07508615e4656e0cab3e02db2c1036e8ceddc
Timtechs <Timmy.s.deckert@gmail.com>
2023-01-04 17:54:40 +0000

    Added function for gameboard, ship placement, player guess, update board and main.

--- run.py
+++ run.py
@@ -17,10 +17,35 @@ def place_ship(size):
     return list(coords)
 
 
+def player_guess():
+    row = int(input("X: \n")) - 1
+    cloumn = int(input("Y: \n")) - 1
+    return (row, column)
+
+
+def update_board(guess, guesses, board, ship):
+    if guess in guesses:
+        print("You already tried that.")
+        return board
+    guesses.append(guess)
+    if guess in ship:
+        print("You hit a ship!")
+        board[guess[0]][guess[1]] = "X"
+        ship.remove(guess)
+        return board
+    print("You missed! Hah!")
+    return board
+
+
 def main():
     board = game_board(5)
     print_board(board)
     ship = place_ship(4); ship
+    x = player_guess(); x
+    guesses = []
+    our_guess = player_guess()
+    board = update_board(our_guess, guesses, board, ship)
+    print_board(board)
 
 
 main()
